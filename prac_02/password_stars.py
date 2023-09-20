d4379dd744fb007c440e185b3122aabd45fe5787
Reece <reece.kelly@my.jcu.edu.au>
2023-09-20 11:27:11 +1000

    Revert "Add prac 1 files"
    This reverts commit 2229da030946d72a7c673f88f9a32569dd13dc14.

--- prac_02/password_stars.py
+++
@@ -1,6 +0,0 @@
-minimum_length = 5
-password = input("Password: ")
-while len(password) < minimum_length:
-    print("Password is too short")
-    password = input("Password: ")
-print("*" * len(password))
