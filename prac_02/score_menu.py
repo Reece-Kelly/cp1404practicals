f1fa99df08816bf4f431e42f447ab948123f3472
Reece <reece.kelly@my.jcu.edu.au>
2023-09-22 15:18:20 +1000

    Add determine result function and print stars function

--- prac_02/score_menu.py
+++ prac_02/score_menu.py
@@ -1,25 +1,30 @@
 MENU = """(G)et a valid score
 (P)rint result
 (S)how stars
-(Q)uit
-"""
+(Q)uit"""
 
 
 def main():
+    score = get_valid_score()
     print(MENU)
     choice = input(">>> ").upper()
     while choice != "Q":
         if choice == "G":
             score = get_valid_score()
         elif choice == "P":
-            pass
+            result = determine_result(score)
+            print(f"Result: {result}")
         elif choice == "S":
-            pass
+            print_stars(score)
         else:
             print("Invalid choice")
         print(MENU)
         choice = input(">>> ").upper()
     print("Thank you and Goodbye!")
+
+
+def print_stars(score):
+    print("*" * score)
 
 
 def get_valid_score():
@@ -28,3 +33,18 @@ def get_valid_score():
         print("Invalid score")
         score = int(input("Score: "))
     return score
+
+
+def determine_result(score):
+    if score < 0 or score > 100:
+        result = "Invalid score"
+    elif score < 50:
+        result = "Bad"
+    elif score < 90:
+        result = "Passable"
+    else:
+        result = "Excellent"
+    return result
+
+
+main()
