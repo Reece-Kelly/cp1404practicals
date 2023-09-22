64e7b456f9be2bf629f93eeb919c19c5c66037a0
Reece <reece.kelly@my.jcu.edu.au>
2023-09-22 16:45:06 +1000

    Add constants to score program and score menu program

--- prac_02/score.py
+++ prac_02/score.py
@@ -4,24 +4,29 @@ Broken program to determine score status
 """
 import random
 
+LOWER_SCORE_LIMIT = 0
+UPPER_SCORE_LIMIT = 100
+PASSING_SCORE_THRESHOLD = 50
+EXCELLENT_SCORE_THRESHOLD = 90
+
 
 def main():
     """Get user score and print result and get random score and print result."""
     user_score = float(input("Enter score: "))
     user_result = determine_result(user_score)
     print(user_result)
-    random_score = random.randint(0, 100)
+    random_score = random.randint(LOWER_SCORE_LIMIT, UPPER_SCORE_LIMIT)
     random_score_result = determine_result(random_score)
     print(f"Random score result: {random_score_result}")
 
 
 def determine_result(score):
     """Determine result based off score."""
-    if score < 0 or score > 100:
+    if score < LOWER_SCORE_LIMIT or score > UPPER_SCORE_LIMIT:
         result = "Invalid score"
-    elif score < 50:
+    elif score < PASSING_SCORE_THRESHOLD:
         result = "Bad"
-    elif score < 90:
+    elif score < EXCELLENT_SCORE_THRESHOLD:
         result = "Passable"
     else:
         result = "Excellent"
--- prac_02/score_menu.py
+++ prac_02/score_menu.py
@@ -2,6 +2,10 @@ MENU = """(G)et a valid score
 (P)rint result
 (S)how stars
 (Q)uit"""
+LOWER_SCORE_LIMIT = 0
+UPPER_SCORE_LIMIT = 100
+PASSING_SCORE_THRESHOLD = 50
+EXCELLENT_SCORE_THRESHOLD = 90
 
 
 def main():
@@ -32,7 +36,7 @@ def print_stars(score):
 def get_valid_score():
     """Get a score between 0 and 100 inclusive."""
     score = int(input("Score: "))
-    while score < 0 or score > 100:
+    while score < LOWER_SCORE_LIMIT or score > UPPER_SCORE_LIMIT:
         print("Invalid score")
         score = int(input("Score: "))
     return score
@@ -40,11 +44,9 @@ def get_valid_score():
 
 def determine_result(score):
     """Determine result based off score."""
-    if score < 0 or score > 100:
-        result = "Invalid score"
-    elif score < 50:
+    if score < PASSING_SCORE_THRESHOLD:
         result = "Bad"
-    elif score < 90:
+    elif score < EXCELLENT_SCORE_THRESHOLD:
         result = "Passable"
     else:
         result = "Excellent"
