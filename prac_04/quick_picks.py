ade3ba72ae2f58b83f0867f6df8f8d72611ae7af
Reece <reece.kelly@my.jcu.edu.au>
2023-10-04 17:23:57 +1000

    Add main function for quick picks program

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -1,9 +1,22 @@
 import random
 
 NUMBER_OF_RANDOM_NUMBERS = 6
+RANDOM_NUMBER_LOWER = 1
+RANDOM_NUMBER_UPPER = 45
 
-number_of_quick_picks = int(input("How many quick picks? "))
-for i in range(number_of_quick_picks):
-    for j in range(NUMBER_OF_RANDOM_NUMBERS):
-        print(random.randint(1, 45), end=" ")
-    print()
+
+def main():
+    number_of_quick_picks = int(input("How many quick picks? "))
+    for i in range(number_of_quick_picks):
+        random_numbers = []
+        for j in range(NUMBER_OF_RANDOM_NUMBERS):
+            random_number = (random.randint(RANDOM_NUMBER_LOWER, RANDOM_NUMBER_UPPER))
+            while random_number in random_numbers:
+                random_number = (random.randint(RANDOM_NUMBER_LOWER, RANDOM_NUMBER_UPPER))
+            random_numbers.append(random_number)
+        random_numbers.sort()
+        print(random_numbers)
+        print()
+
+
+main()
