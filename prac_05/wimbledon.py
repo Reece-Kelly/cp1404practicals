ea72ff25f9b77ccba6f139042f36764cfd2323d7
Reece <reece.kelly@my.jcu.edu.au>
2023-10-11 18:27:46 +1000

    Add docstrings to functions and a constant to the wimbledon program

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -1,10 +1,15 @@
+FILE_NAME = "wimbledon.csv"
+
+
 def main():
-    records = get_records()
+    """Get records from a file and process and display information about records."""
+    records = get_records(FILE_NAME)
     champion_countries, champion_to_count = process_records(records)
     display_processed_records(champion_countries, champion_to_count)
 
 
 def display_processed_records(champion_countries, champion_to_count):
+    """Display the countries of champions and the champions names and how many times they have won"""
     print("Wimbledon Champions:")
     for champion_name, count in champion_to_count.items():
         print(f"{champion_name} {count}")
@@ -13,6 +18,7 @@ def display_processed_records(champion_countries, champion_to_count):
 
 
 def process_records(records):
+    """Return a set of champions countries and a dictionary of champions names to the amount of times they've won"""
     champion_to_count = {}
     champion_countries = set()
     for record in records:
@@ -24,9 +30,10 @@ def process_records(records):
     return champion_countries, champion_to_count
 
 
-def get_records():
+def get_records(file_name):
+    """Read a file and store each line as a list in a list"""
     records = []
-    with open("wimbledon.csv", "r", encoding="utf-8-sig") as in_file:
+    with open(file_name, "r", encoding="utf-8-sig") as in_file:
         in_file.readline()
         for line in in_file:
             record_parts = line.strip().split(",")
