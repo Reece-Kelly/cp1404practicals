ba9476e0c976c2850f29d8cc44be3255673d8370
Reece <reece.kelly@my.jcu.edu.au>
2023-10-11 18:31:47 +1000

    Add main function and dosctring to the emails program

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -1,14 +1,19 @@
-email_to_name = {}
-email = input("Email: ")
-while email != "":
-    username = email.split("@")[0]
-    name_parts = username.split('.')
-    name = " ".join(name_parts).title()
-    name_confirmation_choice = input(f"Is your name {name}? (Y/n) ").upper()
-    if name_confirmation_choice != "Y" and name_confirmation_choice != "":
-        name = input("Name: ")
-    email_to_name[email] = name
+def main():
+    """Get a name from an email and create a dictionary of email to names."""
+    email_to_name = {}
     email = input("Email: ")
-for email, name in email_to_name.items():
-    print(f"{name} ({email})")
+    while email != "":
+        username = email.split("@")[0]
+        name_parts = username.split('.')
+        name = " ".join(name_parts).title()
+        name_confirmation_choice = input(f"Is your name {name}? (Y/n) ").upper()
+        if name_confirmation_choice != "Y" and name_confirmation_choice != "":
+            name = input("Name: ")
+        email_to_name[email] = name
+        email = input("Email: ")
+    for email, name in email_to_name.items():
+        print(f"{name} ({email})")
+
+
+main()
 
