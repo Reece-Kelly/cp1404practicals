a49500a4aa50ae6d6cb377e26cc54e0837ab52ae
Reece <reece.kelly@my.jcu.edu.au>
2023-10-11 18:37:45 +1000

    Add main function and docstring to word occurrences program

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -4,10 +4,16 @@ Estimate: 30 minutes
 Actual: 32 minutes
 """
 
-string = input("String: ")
-words = string.split(" ")
-words.sort()
-word_to_count = {word: (words.count(word)) for word in words}
-max_length = len(max(words))
-for word, count in word_to_count.items():
-    print(f"{word:{max_length}} : {count}")
+
+def main():
+    """Get a string and display the amount of times each word occurs in that string."""
+    string = input("String: ")
+    words = string.split(" ")
+    words.sort()
+    word_to_count = {word: (words.count(word)) for word in words}
+    max_length = len(max(words))
+    for word, count in word_to_count.items():
+        print(f"{word:{max_length}} : {count}")
+
+
+main()
