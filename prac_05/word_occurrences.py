73a6c1fcefd5674cba23629d050e9c05bba42a4d
Reece <reece.kelly@my.jcu.edu.au>
2023-10-11 11:39:23 +1000

    Finish word occurrences program

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -1,11 +1,13 @@
 """
 Word Occurrences
 Estimate: 30 minutes
-Actual:
+Actual: 32 minutes
 """
 
 string = input("String: ")
 words = string.split(" ")
 words.sort()
 word_to_count = {word: (words.count(word)) for word in words}
-print(word_to_count)
+max_length = len(max(words))
+for word, count in word_to_count.items():
+    print(f"{word:{max_length}} : {count}")
