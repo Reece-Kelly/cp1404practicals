6185d62580b2be3cd86c9fd996e92db6af14bf2c
Reece <reece.kelly@my.jcu.edu.au>
2023-10-25 11:09:09 +1000

    Add a __str__ method

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -1,5 +1,5 @@
 class ProgrammingLanguage:
-    def __int__(self, name, typing, reflection, year):
+    def __init__(self, name, typing, reflection, year):
         self.name = name
         self.typing = typing
         self.reflection = reflection
@@ -7,3 +7,6 @@ class ProgrammingLanguage:
 
     def is_dynamic(self):
         return self.typing == "Dynamic"
+
+    def __str__(self):
+        return f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}"
