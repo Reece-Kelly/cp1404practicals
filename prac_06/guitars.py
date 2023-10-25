2b3488b7c8ba1bf7672e2a69a6290d497da68c97
Reece <reece.kelly@my.jcu.edu.au>
2023-10-25 23:51:28 +1000

    Add loop that prints each guitar in guitars list

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -1,13 +1,27 @@
 from guitar import Guitar
 
-print("My guitars!")
-guitars = []
-name = input("Name: ")
-while name != "":
-    year = int(input("Year: "))
-    cost = float(input("Cost: "))
-    guitar = Guitar(name, year, cost)
-    guitars.append(guitar)
-    print(f"{name} ({year}) : ${cost} added.\n")
+
+def main():
+    """Program that gets guitars from user and prints them neatly."""
+    print("My guitars!")
+    guitars = []
     name = input("Name: ")
-print("\n... snip ...\n")
+    while name != "":
+        year = int(input("Year: "))
+        cost = float(input("Cost: "))
+        guitar = Guitar(name, year, cost)
+        guitars.append(guitar)
+        print(f"{name} ({year}) : ${cost} added.\n")
+        name = input("Name: ")
+    # guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
+    # guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
+    print("These are my guitars:")
+    for i, guitar in enumerate(guitars, 1):
+        if guitar.is_vintage():
+            vintage_string = " (vintage)"
+        else:
+            vintage_string = ""
+        print(f"Guitar {i}: {guitar.name:>20} ({guitar.year}), worth ${guitar.cost:10.2f}{vintage_string}")
+
+
+main()
