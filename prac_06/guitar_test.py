1891020cfb82f8f85404e662f65ae6c9eba4ffb5
Reece <reece.kelly@my.jcu.edu.au>
2023-10-26 00:21:53 +1000

    Update print statement to include variable for name rather than string in guitar test program

--- prac_06/guitar_test.py
+++ prac_06/guitar_test.py
@@ -5,10 +5,10 @@ def main():
     gibson = Guitar("Gibson", 1922, 16035.40)
     epiphone = Guitar("Epiphone", 2004, 500)
 
-    print(f"Gibson get_age() - Expected 101. Got {gibson.get_age()}")
-    print(f"Epiphone get_age() - Expected 19. Got {epiphone.get_age()}")
-    print(f"Gibson is_vintage() - Expected True. Got {gibson.is_vintage()}")
-    print(f"Epiphone is_vintage() - Expected False. Got {epiphone.is_vintage()}")
+    print(f"{gibson.name} get_age() - Expected 101. Got {gibson.get_age()}")
+    print(f"{epiphone.name} get_age() - Expected 19. Got {epiphone.get_age()}")
+    print(f"{gibson.name} is_vintage() - Expected True. Got {gibson.is_vintage()}")
+    print(f"{epiphone.name} is_vintage() - Expected False. Got {epiphone.is_vintage()}")
 
 
 main()
