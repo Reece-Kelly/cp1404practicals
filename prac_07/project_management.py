dc582e007e7b457258ed68cd58f128b7c443c63f
Reece <reece.kelly@my.jcu.edu.au>
2023-11-01 12:41:07 +1000

    Add basic menu structure to project_management.py

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -2,4 +2,34 @@
 Estimate time to complete: 2 hours
 Actual time to complete:
 """
+MENU = ("- (L)oad projects\n"
+        "- (S)ave projects\n"
+        "- (D)isplay projects\n"
+        "- (F)ilter projects by date\n"
+        "- (A)dd new project\n"
+        "- (U)pdate project\n"
+        "- (Q)uit")
 
+
+def main():
+    print(MENU)
+    choice = input(">>> ").upper()
+    while choice != "Q":
+        if choice == "L":
+            print("L")
+        elif choice == "S":
+            print("S")
+        elif choice == "D":
+            print("D")
+        elif choice == "F":
+            print("F")
+        elif choice == "A":
+            print("A")
+        elif choice == "U":
+            print("U")
+        else:
+            print("Invalid choice.")
+        choice = input(">>> ").upper()
+
+
+main()
