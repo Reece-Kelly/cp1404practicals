d42983a57df97b0cf66814f1428dcdbe98b3aaee
Reece <reece.kelly@my.jcu.edu.au>
2023-11-03 14:16:42 +1000

    Update project.py to include datetime module

--- prac_07/project.py
+++ prac_07/project.py
@@ -1,7 +1,10 @@
+import datetime
+
+
 class Project:
     def __init__(self, name, start_date, priority, cost_estimate, completion_percentage):
         self.name = name
-        self.start_date = start_date
+        self.start_date = datetime.datetime.strptime(start_date, "%d/%m/%Y").date()
         self.priority = priority
         self.cost_estimate = cost_estimate
         self.completion_percentage = completion_percentage
