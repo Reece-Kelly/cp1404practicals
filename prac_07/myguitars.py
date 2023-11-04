a1ccb33af8fe369e8e554604711a0799daecb87f
Reece <reece.kelly@my.jcu.edu.au>
2023-11-04 18:05:47 +1000

    Refactor main function into two smaller functions in myguitars.py

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -1,19 +1,19 @@
 """
-Estimate to complete: 30 minutes
-Start time: 11:10
-Finish time:
+Estimate time to complete: 30 minutes
+Actual time to complete: 42 minutes
 """
 from guitar import Guitar
 
 
 def main():
-    guitars = []
-    in_file = open("guitars.csv", "r")
-    for line in in_file:
-        guitar_details = line.strip().split(",")
-        guitar = Guitar(guitar_details[0], guitar_details[1], guitar_details[2])
-        guitars.append(guitar)
-    in_file.close()
+    """
+    Load and display a list of guitars from a file and allow the user to add to the list before saving it to a file.
+    """
+    guitars = load_guitars()
+    print("Unsorted guitars:")
+    for guitar in guitars:
+        print(guitar)
+    print("\nSorted guitars:")
     guitars.sort()
     for guitar in guitars:
         print(guitar)
@@ -25,10 +25,28 @@ def main():
         guitars.append(guitar)
         print(f"{guitar} added.\n")
         name = input("Name: ")
-    # out_file = ("guitars.csv", "w")
-    # for guitar in guitars:
-    #     print(guitar)
-    # out_file.close()
+    save_guitars(guitars)
+
+
+def load_guitars():
+    """Load a list of guitars from a file and create a list of guitar objects."""
+    guitars = []
+    in_file = open("guitars.csv", "r")
+    for line in in_file:
+        guitar_details = line.strip().split(",")
+        guitar = Guitar(guitar_details[0], guitar_details[1], guitar_details[2])
+        guitars.append(guitar)
+    in_file.close()
+    return guitars
+
+
+def save_guitars(guitars):
+    """Save a list of guitar objects to a file."""
+    out_file = open("guitars.csv", "w")
+    for guitar in guitars:
+        string = f"{guitar.name},{guitar.year},{guitar.cost}\n"
+        out_file.write(string)
+    out_file.close()
 
 
 main()
