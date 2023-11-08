44cc91689c3ca5bd195adac3cd0a62f342a44c77
Reece <reece.kelly@my.jcu.edu.au>
2023-11-08 12:14:53 +1000

    Change window size in  convert_miles_km.py

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -6,7 +6,7 @@ from kivy.core.window import Window
 class MilesToKilometresApp(App):
 
     def build(self):
-        Window.size = (200, 100)
+        Window.size = (800, 500)
         self.title = "Convert Miles to Kilometres"
         self.root = Builder.load_file('convert_miles_km.kv')
         return self.root
