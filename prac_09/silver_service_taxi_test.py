aa9c38b68c912a3dcaf93b161a28f222418efa82
Reece <reece.kelly@my.jcu.edu.au>
2023-11-15 11:20:30 +1000

    Add tests to silver_service_taxi_test.py

---
+++ prac_09/silver_service_taxi_test.py
@@ -0,0 +1,6 @@
+from silver_service_taxi import SilverServiceTaxi
+
+test_taxi = SilverServiceTaxi("Silver Service Taxi 1", 100, 2)
+test_taxi.drive(18)
+print(test_taxi)
+print(test_taxi.get_fare())
