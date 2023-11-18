5352440d4d15f59938287e99239ca48618a0f40a
Reece <reece.kelly@my.jcu.edu.au>
2023-11-18 19:52:39 +1000

    Add code to "c" and "d" menu options in taxi_simulator.py

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -1,3 +1,4 @@
+from car import Car
 from silver_service_taxi import SilverServiceTaxi
 from taxi import Taxi
 
@@ -5,14 +6,32 @@ MENU = "q)uit, c)hoose taxi, d)rive"
 
 
 def main():
+    taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2), SilverServiceTaxi("Hummer", 200, 4)]
+    total_bill = 0
     print("Lets drive!")
     print(MENU)
     user_choice = input(">>> ").lower()
     while user_choice != "q":
-        if user_choice == "d":
-            pass
         if user_choice == "c":
-            pass
+            print("Taxis available:")
+            for i, taxi in enumerate(taxis):
+                print(f"{i} - {taxi}")
+            taxi_choice = int(input("Choose taxi: "))
+            chosen_taxi = taxis[taxi_choice]
+        elif user_choice == "d":
+            if chosen_taxi:
+                chosen_taxi.start_fare()
+                distance_to_drive = float(input("Drive how far? "))
+                chosen_taxi.drive(distance_to_drive)
+                print(f"Your {chosen_taxi.name} trip cost you ${chosen_taxi.get_fare()}")
+                total_bill += chosen_taxi.get_fare()
+            else:
+                print("You need to choose a taxi before you can drive")
+        else:
+            print("Invalid option")
+        print(f"Bill to date: ${total_bill}")
         print(MENU)
         user_choice = input(">>> ").lower()
 
+
+main()
