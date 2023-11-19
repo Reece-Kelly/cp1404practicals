d2be996b574ea413f79df47337bdc5ebc8f38636
Reece <reece.kelly@my.jcu.edu.au>
2023-11-19 13:38:17 +1000

    Add display taxis function to taxi_simulator.py

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -15,8 +15,7 @@ def main():
     while user_choice != "q":
         if user_choice == "c":
             print("Taxis available:")
-            for i, taxi in enumerate(taxis):
-                print(f"{i} - {taxi}")
+            display_taxis(taxis)
             taxi_choice = int(input("Choose taxi: "))
             try:
                 chosen_taxi = taxis[taxi_choice]
@@ -38,7 +37,12 @@ def main():
         user_choice = input(">>> ").lower()
     print(f"Total trip cost: ${total_bill}")
     print("Taxis are now:")
+    display_taxis(taxis)
+
+
+def display_taxis(taxis):
     for i, taxi in enumerate(taxis):
         print(f"{i} - {taxi}")
+
 
 main()
